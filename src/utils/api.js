9bf474f02f9decc03e1436297bb2533aa5f2d13b
Satyam Raj <rajsatyammm@gmail.com>
2023-09-28 15:57:40 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const options = {
     params: { hl: "en", gl: "IN" },
     headers: {
         "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "01e028cd70msha805ff00857d4a4p1d1b82jsnf111aca09ffa",
+            process.env.REACT_APP_YOUTUBE_API_KEY,
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
